1cf05eea571bcf2ae9558d4579c8357da1ef8b4a
JeffRisberg <jrisberg@aol.com>
2018-07-15 23:07:56 -0700

    added Simple Launch example

--- src/main/java/com/company/examples/SimpleLaunchExample.java
+++ src/main/java/com/company/examples/SimpleLaunchExample.java
@@ -4,9 +4,7 @@ import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
 import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
-import io.kubernetes.client.models.V1Container;
-import io.kubernetes.client.models.V1Pod;
-import io.kubernetes.client.models.V1PodSpec;
+import io.kubernetes.client.models.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,19 +22,28 @@ public class SimpleLaunchExample {
 
     CoreV1Api apiInstance = new CoreV1Api();
 
-    V1Pod body = new V1Pod();
-    V1PodSpec podSpec = new V1PodSpec();
-    body.setSpec(podSpec);
+    V1ObjectMeta metadata = new V1ObjectMeta();
+    metadata.setName("kuard");
 
-    List<V1Container> containers = new ArrayList<>();
     V1Container container = new V1Container();
-    container.setImage("kurad/kuar-demo/kuar-am64:1");
+    container.setImage("gcr.io/kuar-demo/kuard-amd64:1");
     container.setName("kuard");
 
+    V1ContainerPort port = new V1ContainerPort();
+    port.setContainerPort(8080);
+    port.setName("http");
+    port.setProtocol("HTTP");
+    container.addPortsItem(port);
+
+    V1PodSpec podSpec = new V1PodSpec();
     podSpec.addContainersItem(container);
 
+    V1Pod pod = new V1Pod();
+    pod.setMetadata(metadata);
+    pod.setSpec(podSpec);
+
     try {
-      V1Pod result = apiInstance.createNamespacedPod(namespace, body, pretty);
+      V1Pod result = apiInstance.createNamespacedPod(namespace, pod, pretty);
       System.out.println(result);
     } catch (ApiException e) {
       System.err.println("Exception when calling CoreV1Api#createNamespacedPod");
